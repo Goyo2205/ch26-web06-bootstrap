3426edfb15383795a0ed71bc5984d58ce652666a
goyo2205 <jgregorio.ef@gmail.com>
2023-04-22 19:01:41 -0600

    cambio de la nav bar en pag mi nombre

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,24 @@
 console.log("Hola desde un script externo");
 
-function changeColorRed(){
-    const refName = document.getElementById("name");
-    refName.style.color = "red";
+function changeColor(color){
+    const refName = getReferenceFullName();
+    refName.style.color = color;
+}
+
+function getReferenceFullName(){
+    return document.getElementById("name")
+}
+
+function changeColorParagraph(color, refObj){
+    console.log(refObj);
+    refObj.style.color = color;
+}
+function resetColor(color){
+    const refObjs = document.getElementsByClassName("text-color")
+    console.log(refObjs);
+    for (let i = 0; i < refObjs.length; i++) {
+        const element = refObjs[i];
+        element.style.color = "black"
+    }
+    
 }
